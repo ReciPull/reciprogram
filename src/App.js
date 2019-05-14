4eb4cae965e215724cc394398a6d1f4d127b6528
Austin Wong <austinwong@umail.ucsb.edu>
2019-05-14 16:52:28 -0700

    search recipe button

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './App.css'
+import IngredientSection from './components/IngredientSection';
+import logo from './components/imgs/recipull_circle.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import CardSection from './components/Card/CardSection';
+
+class App extends React.Component {
+    render() {
+        return (
+          <div className="App">
+            <div className="TitleBar">
+                <img src={logo}/>
+            </div>
+            <div className="MainSectors">
+                <div className="IngredientSector">
+                    
+                    <IngredientSection />
+                </div>
+                <div className="CardSector">
+                    <CardSection />
+                </div>
+            </div>
+          </div>
+        );
+      }
 }
 
-export default App;
+export default App; 
