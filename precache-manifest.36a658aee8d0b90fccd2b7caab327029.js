f5f5ee8f7a2b62b2f40df1b5d0e831b810c82283
Austin Wong <austinwong@umail.ucsb.edu>
2019-05-28 23:41:27 -0700

    Updates

---
+++ precache-manifest.36a658aee8d0b90fccd2b7caab327029.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02c411646c7c778ec956e603427d847a",
+    "url": "/reciprogram/index.html"
+  },
+  {
+    "revision": "27dd088236bdb58ec055",
+    "url": "/reciprogram/static/css/main.414fcf0d.chunk.css"
+  },
+  {
+    "revision": "5c1ff29d1c02c7cf6335",
+    "url": "/reciprogram/static/js/2.f848538a.chunk.js"
+  },
+  {
+    "revision": "27dd088236bdb58ec055",
+    "url": "/reciprogram/static/js/main.95a039a5.chunk.js"
+  },
+  {
+    "revision": "8a3a2c9270296567307f",
+    "url": "/reciprogram/static/js/runtime~main.aab35674.js"
+  },
+  {
+    "revision": "cf5917f1b1be4497ac84d94b6e0ef73f",
+    "url": "/reciprogram/static/media/recipull_circle.cf5917f1.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0646e9f72c689de077e5e2d975c89545.js"
+  "/reciprogram/precache-manifest.36a658aee8d0b90fccd2b7caab327029.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reciprogram/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
